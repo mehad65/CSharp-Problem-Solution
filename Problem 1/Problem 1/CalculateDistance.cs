60de9120cec86007876eb68110c4bead193cb7fc
mehad65 <mehad65@gmail.com>
2022-03-07 21:14:25 +0600

    commit final

---
+++ Problem 1/Problem 1/CalculateDistance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    internal class CalculateDistance
+    {
+        public double Calculate(int x1,int y1, int x2, int y2)
+        {
+            double distance=Math.Sqrt(Math.Pow((x2 - x1),2)+Math.Pow((y2-y1),2));
+            return distance;
+        }
+    }
+}
---
+++ Problem 1/Problem 1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter A points Location");
+string A=Console.ReadLine(); 
+Console.WriteLine("Enter B points location");
+string B=Console.ReadLine(); 
+string[] Apoint = A.Split(',');
+string[] Bpoint = B.Split(",");
+int x1=int.Parse(Apoint[0]);   
+int y1=int.Parse(Apoint[1]);
+int x2=int.Parse(Bpoint[0]);
+int y2=int.Parse(Apoint[1]);
+double distance= Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2));
+
+Console.WriteLine("Distance = " + distance);
