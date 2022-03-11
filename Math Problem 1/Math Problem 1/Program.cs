0566c36e1c95d6193fd4ae8b3815530946ea7d17
mehad65 <mehad65@gmail.com>
2022-03-12 00:01:59 +0600

    ok

---
+++ Math Problem 1/Math Problem 1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Please enter a number: ");
+int input=int.Parse(Console.ReadLine());
+double squareRoot=Math.Sqrt(input);
+double result=Math.Round(squareRoot);
+
+Console.WriteLine(result);
+
+
