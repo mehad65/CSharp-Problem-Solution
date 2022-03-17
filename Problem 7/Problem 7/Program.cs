30b9b813a25b31cc48f12cd3b409242f9662039f
mehad65 <mehad65@gmail.com>
2022-03-17 12:36:45 +0600

    Problem 7

---
+++ Problem 7/Problem 7/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Please enter numbers with comma separator");
+string input=Console.ReadLine(); 
+string[] numbers=input.Split(',');
+int[] intnumbers=new int[numbers.Length];
+string k;
+for(int i=0; i<numbers.Length; i++)
+{
+    for (int j = 0; j < numbers.Length-1; j++)
+    {
+        if (int.Parse(numbers[i]) < int.Parse(numbers[j]))
+        {
+            k = numbers[i];
+            numbers[i]=numbers[j];
+            numbers[j]=k;
+        }
+    }
+}
+foreach (var item in numbers)
+{
+    string comma = item.Equals(numbers.LastOrDefault()) ? " " : ", ";
+    Console.Write(item +comma);
+}
+
--- README.md
+++
@@ -1,1 +0,0 @@
-# CSharp-Problem-Solution
