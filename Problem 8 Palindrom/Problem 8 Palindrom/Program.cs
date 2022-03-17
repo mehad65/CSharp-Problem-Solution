9e6f6dd6935e69dd1f168996ca56331e843000d3
mehad65 <mehad65@gmail.com>
2022-03-17 15:42:03 +0600

    ok

---
+++ Problem 8 Palindrom/Problem 8 Palindrom/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Please enter a word");
+var input=Console.ReadLine();
+char[] inputarr=input.ToCharArray();
+inputarr.Reverse();
+string output=new string(inputarr);
+if (input.ToLower() == output.ToLower())
+{
+    Console.WriteLine("Your text is Palindrom");
+}
+else
+{
+    Console.WriteLine("Your text is not palindrom");
+}
