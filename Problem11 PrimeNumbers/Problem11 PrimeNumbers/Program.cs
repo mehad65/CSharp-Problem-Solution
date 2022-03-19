1174c22a7a34fe26da680c64d0878fb721ebbd11
mehad65 <mehad65@gmail.com>
2022-03-19 14:13:07 +0600

    Prime Numbers

---
+++ Problem11 PrimeNumbers/Problem11 PrimeNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Please Enter a Number");
+long input=long.Parse(Console.ReadLine());
+bool result=true;
+for (int i = 2; i <= input/2; i++)
+{
+    if (input % i == 0)
+    {
+        result=false;
+        break;
+    }
+}
+Console.WriteLine(result);
+
